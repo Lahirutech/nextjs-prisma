fb244e0c54021d2006764627f5584b54fde30472
Lanka Rathnayaka <rmllbrathnayaka@gmail.com>
2024-08-23 12:15:08 +1000

    initialize

---
+++ app/api/getusers/route.ts
@@ -0,0 +1,14 @@
+import prisma from "@/utils/connect";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    const users = await prisma.user.findMany();
+    return new NextResponse(JSON.stringify({ message: users, status: 200 }));
+  } catch (err) {
+    console.log("error", err);
+    return new NextResponse(
+      JSON.stringify({ message: "get users failed", status: 500 })
+    );
+  }
+};
