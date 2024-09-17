4484d92075d6a435900181262f81d1425228f290
Lanka Rathnayaka <lanka@symbiote.com.au>
2024-09-17 20:41:56 +1000

    update url to test

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -15,17 +15,12 @@ interface GetUsersResponse {
 export async function getUsers(): Promise<GetUsersResponse> {
     try {
         // Ensure we're using an absolute URL
-        const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'https';
+        const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
         const host = headers().get('host') || 'localhost:3000';
-        const url = `${protocol}://${host}/api/getusers`;
+        const url = `https://nextjs-prisma.pages.dev/api/getusers`;
         console.log("🚀 ~ getUsers ~ host:", host)
 
-        const response = await fetch(url, {
-            cache: 'no-store',
-            headers: {
-                'Cookie': headers().get('cookie') ?? ''
-            }
-        });
+        const response = await fetch(url);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
